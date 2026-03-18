998862dcd41629b4470c8bb4127eccaac0037099
emilieChnvt <emilie.chnvt@gmail.com>
2026-03-18 14:30:33 +0100

    about section added with scoll animations

--- exercices/onePage/main.ts
+++ exercices/onePage/main.ts
@@ -1,4 +1,39 @@
 import "@/style.css";
 import "./style.css";
+import gsap from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
 
-console.log("exemple");
+gsap.registerPlugin(ScrollTrigger)
+
+const titleTimeline = gsap.timeline();
+
+titleTimeline
+    .from('.title', { xPercent: 110, opacity: 0, duration: 1 })
+    .from('header .description', { xPercent: 130, opacity: 0, duration: 1 }, "-=0.5"); // commence 0.5s avant la fin du titre
+
+titleTimeline.play();
+
+
+const aboutTimeline = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#about",
+        start: "top 80%",
+    }
+});
+
+aboutTimeline
+    .from('#about h2', {
+    yPercent: 50,
+    opacity: 0,
+    duration: 1
+})
+    .from('#about .description', {
+    x: -100,
+    opacity: 0,
+    duration: 1
+}, "-=0.5")
+    .from('#about img', {
+    x: 100,
+    opacity: 0,
+    duration: 1
+}, "-=0.3");
