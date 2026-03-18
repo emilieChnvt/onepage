4818a43ce8a2b97921f1cffba86cd3629b34a6ad
emilieChnvt <emilie.chnvt@gmail.com>
2026-03-18 15:15:28 +0100

    about serum added with animations scroll

--- exercices/onePage/main.ts
+++ exercices/onePage/main.ts
@@ -13,27 +13,42 @@ titleTimeline
 
 titleTimeline.play();
 
-
 const aboutTimeline = gsap.timeline({
     scrollTrigger: {
         trigger: "#about",
         start: "top 80%",
+        toggleActions: "play none none reverse",
     }
 });
 
 aboutTimeline
-    .from('#about h2', {
-    yPercent: 50,
-    opacity: 0,
-    duration: 1
-})
-    .from('#about .description', {
-    x: -100,
-    opacity: 0,
-    duration: 1
-}, "-=0.5")
-    .from('#about img', {
-    x: 100,
-    opacity: 0,
-    duration: 1
-}, "-=0.3");
+    .from('#about h2', { yPercent: 50, opacity: 0, duration: 1 })
+    .from('#about .description', { x: -100, opacity: 0, duration: 1 }, "-=0.5")
+    .from('#about img', { x: 100, opacity: 0, duration: 1 }, "-=0.3");
+
+const serumTimeline = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#about_serum",
+        start: "top 80%",
+        toggleActions: "play none none reverse",
+    }
+});
+
+serumTimeline
+    .from('#about_serum h2', { yPercent: 50, opacity: 0, duration: 1 })
+    .from('#about_serum .text-left p', { x: -100, opacity: 0, duration: 1 }, "-=0.5");
+
+gsap.to(".triangle", {
+    rotation: 360,
+    duration: 10,
+    repeat: -1,
+
+    ease: "linear"
+});
+
+gsap.to(".circle ", {
+    rotation: -360,
+    duration: 10,
+    repeat: -1,
+    ease: "linear"
+});
